335b2d2d838dc1a16bd9dffa2c360b3a9b45faef
pamcuellas <pamcuellas@gmail.com>
2019-08-24 22:58:15 -0400

    Traying new code.

--- scrape_mars.py
+++ scrape_mars.py
@@ -18,9 +18,22 @@ import pandas as pd
 
 def init_browser():
     # /usr/local/bin/chromedriver
-    
-    executable_path = {"executable_path": "/usr/bin/google-chrome"}
-    return Browser("chrome", **executable_path, headless=False)
+
+    CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+    # CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+
+    chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+    options = webdriver.ChromeOptions()
+    options.binary_location = chrome_bin
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x600')
+    return webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+
+
+    # executable_path = {"executable_path": "/usr/bin/google-chrome"}
+    # return Browser("chrome", **executable_path, headless=False)
 
     # executable_path = {"executable_path": "chromedriver.exe"}
     # return Browser("chrome", **executable_path, headless=False)
