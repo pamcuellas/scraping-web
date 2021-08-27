1f5402ba73cf8a5f9ea172bc6d8db7b4149a701e
pamcuellas <pamcuellas@gmail.com>
2021-08-27 11:33:39 -0400

    URL mars images updated.

--- scrape_mars.py
+++ scrape_mars.py
@@ -150,10 +150,10 @@ def scrape():
     # to obtain high resolution images for each of Mar"s hemispheres.
     # url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     hemisphere_image_urls =[]
-    hemisphere_image_urls.append({"title":"Cerberus Hemisphere",        "img_url": "https://astrogeology.usgs.gov/cache/images/cfa62af2557222a02478f1fcd781d445_cerberus_enhanced.tif_full.jpg"}) 
-    hemisphere_image_urls.append({"title":"Schiaparelli Hemisphere",    "img_url": "https://astrogeology.usgs.gov/cache/images/3cdd1cbf5e0813bba925c9030d13b62e_schiaparelli_enhanced.tif_full.jpg"}) 
-    hemisphere_image_urls.append({"title":"Syrtis Major Hemisphere",    "img_url": "https://astrogeology.usgs.gov/cache/images/ae209b4e408bb6c3e67b6af38168cf28_syrtis_major_enhanced.tif_full.jpg"}) 
-    hemisphere_image_urls.append({"title":"Valles Marineris Hemisphere","img_url": "https://astrogeology.usgs.gov/cache/images/7cf2da4bf549ed01c17f206327be4db7_valles_marineris_enhanced.tif_full.jpg"}) 
+    hemisphere_image_urls.append({"title":"Cerberus Hemisphere",        "img_url": "https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg"}) 
+    hemisphere_image_urls.append({"title":"Schiaparelli Hemisphere",    "img_url": "https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"}) 
+    hemisphere_image_urls.append({"title":"Syrtis Major Hemisphere",    "img_url": "https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"}) 
+    hemisphere_image_urls.append({"title":"Valles Marineris Hemisphere","img_url": "https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"}) 
     
     # browser.visit(url)
     # html = browser.html
